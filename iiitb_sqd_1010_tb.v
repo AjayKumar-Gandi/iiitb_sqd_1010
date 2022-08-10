7f7240d8ff4c797827ff66f962d0a802c76df557
AjayKumar-Gandi <110395336+AjayKumar-Gandi@users.noreply.github.com>
2022-08-10 23:22:23 +0530

    Update iiitb_sqd_1010_tb.v

--- iiitb_sqd_1010_tb.v
+++ iiitb_sqd_1010_tb.v
@@ -4,11 +4,13 @@ wire y;
 iiitb_sqd_1010 m1(din, reset, clk, y);
 initial
 begin
+$dumpfile("iiitb_sqd_tb.vcd");
+$dumpvars(0);
 reset=0       ;clk=0;din=0;
 $monitor($time, , ,"c=%b",clk,,"y=%b",y,,"r=%b",reset,,"d=%b",din);
-#5 din=1;
-#5 din=1;
-#5 din=1;
+#10 din=1;
+#10 din=0;
+#10 din=1;
 #10 din=0;
 #10 din=1;
 #10 din=0;
